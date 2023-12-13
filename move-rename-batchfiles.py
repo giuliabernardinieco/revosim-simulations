f1e106db844c5185cfb12a424c637385e6d531fd
Giulia Bernardini <giuliaex@gmail.com>
2023-12-13 17:43:10 +0000

    moving more files

--- move-rename-batchfiles.py
+++ move-rename-batchfiles.py
@@ -1,14 +1,14 @@
 import os
 
-source_folder = "/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/2nd batches - Blue channel/1over200/mh"
-destination_folder = "/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/1over200/mh"
+source_folder = "/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/2nd batches - Blue channel/1over50-bis/mh"
+destination_folder = "/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/1over50/mh"
 
 #checking if the folder exists, otherwise create one
 os.makedirs(destination_folder, exist_ok=True)
 
 for i in range(1, 31):
     old_filename = f"REvoSim_log_{i}.txt"
-    new_filename = f"REvoSim_log_{i + 50}.txt"
+    new_filename = f"REvoSim_log_{i + 80}.txt"
     source_path = os.path.join(source_folder, old_filename)
     destination_path = os.path.join(destination_folder, new_filename)
     
@@ -17,7 +17,7 @@ for i in range(1, 31):
 
 for i in range(1, 31):
     old_filename = f"REvoSim_individuals_data_{i}.txt"
-    new_filename = f"REvoSim_individuals_data_{i + 50}.txt"
+    new_filename = f"REvoSim_individuals_data_{i + 80}.txt"
     source_path = os.path.join(source_folder, old_filename)
     destination_path = os.path.join(destination_folder, new_filename)
     
