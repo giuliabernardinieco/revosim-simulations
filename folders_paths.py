d59835bf6579acfdd4bd338f1c77ad224f9a57cb
Giulia Bernardini <giuliaex@gmail.com>
2024-03-14 11:04:47 +0000

    implementing separate folder file system

---
+++ folders_paths.py
@@ -0,0 +1,69 @@
+###########################################################################################################
+############################################################################################################
+# These folders contain simulations run with a toriodal environent and dispersion parameter of 15. 
+# The environment is sin function on blue, and 0 on both red and green.
+# Basically I re-run these because I realised my environments were wrong and not perfectly toriodal and also 
+# I preferred to have 0 on red and green, while before my environments were 0 on red and 127 on green 
+
+# List of folder paths
+folder_paths = ['/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/w5',
+                '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/w10',
+                '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/w20',
+                '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/w25',
+                '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/w50',
+                '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/w100',]
+# Define folder labels
+folder_labels = [5, 10, 20, 25, 50, 100]
+
+
+# ############################################################################################################
+# ############################################################################################################
+# # These folders contain simulations run with a toriodal environent and dispersion parameter of 15. 
+# # The environment is sin function on blue, and 0 on both red and green.
+# # This folder contains 2000 simulations I run on the cluster
+
+# # List of folder paths
+# folder_paths = ['/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/w50_1000',]
+# # Define folder labels
+# folder_labels = [50]
+# ############################################################################################################
+# ############################################################################################################
+
+
+# ###########################################################################################################
+# ############################################################################################################
+# #Define the folders containing the csv files
+# # List of folder paths
+# folder_paths = ['/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/1over5/mh',
+#     '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/1over10/mh',
+#     '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/1over25/mh',
+#     '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/1over50/mh',
+#     '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/1over100/mh',]
+# # Define folder labels
+# folder_labels = [5, 10, 25, 50, 100]
+# ###########################################################################################################
+# ############################################################################################################
+
+
+# ###########################################################################################################
+# ############################################################################################################
+# #freq that are on blue as sin, red as 0 and green on 127, 64 BITS GENOME
+# folder_paths = ['/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/64-bits-genome/5w_64_bits',
+#                 '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/64-bits-genome/10w_64_bits',
+#                 '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/64-bits-genome/25w_64_bits',
+#                 '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/64-bits-genome/50w_64_bits',
+#                 '/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/64-bits-genome/100w_64_bits',]
+# # Define folder labels
+# folder_labels = [ 5, 10, 25, 50, 100]
+# ###########################################################################################################
+# ############################################################################################################
+
+
+# ###########################################################################################################
+# ############################################################################################################
+# #freq that are on blue as sin, red as 0 and green on 127
+# folder_paths = ['/Users/gb4818/Library/CloudStorage/OneDrive-ImperialCollegeLondon/Rev-res-in/3rd batches - Blue and Green/1over50/mh',]
+# # Define folder labels
+# folder_labels = [50]
+# ###########################################################################################################
+# ############################################################################################################
