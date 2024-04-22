969477e310c0ea40feea9a984e80ee4d2cde44dd
Giulia Bernardini <giuliaex@gmail.com>
2024-04-22 23:43:43 +0100

    Sameer's magic

--- fetch-data-genomes-differences.py
+++ fetch-data-genomes-differences.py
@@ -4,6 +4,7 @@ import pandas as pd
 import re
 from tqdm import tqdm
 import time
+from sklearn.metrics.pairwise import pairwise_distances
 
 # Folder Paths
 # List of folder paths
@@ -26,10 +27,17 @@ with open(out_filename, "w") as out_file:
 ###LOGS SPECIES COUNT###
 ########################
 
-def genome_string_difference_count(genome_1: str, genome_2: str) -> int:
-    return sum(c1 != c2 for c1, c2 in zip(genome_1, genome_2))
 
-# Creating a function to create and store diversity values and return them in a table, 
+def genome_string_difference_count(genome_1: str, genome_2: str, cache: dict[str, str]) -> int:
+    key = f"{genome_1}-{genome_2}"
+    reverse_key = f"{genome_2}-{genome_1}"
+    if key not in cache:
+        cache[key] = sum(c1 != c2 for c1, c2 in zip(genome_1, genome_2))
+        cache[reverse_key] = cache[key]
+    return cache[key]
+
+
+# Creating a function to create and store diversity values and return them in a table,
 # from this table we will calculate stats for each sample
 def diversity_table(genomes, num_pairs):
     # Select random pairs of indices
@@ -63,6 +71,8 @@ for folder_i, folder_path in tqdm(enumerate(folder_paths)):
     time_taken = time.perf_counter() - start
     print(f"# of files = {len(files_to_read)}")
     print(f"time taken = {time_taken:0.3f}s")
+    differences_list = []
+    difference_cache = {}
 
     for file_j, filename in tqdm(enumerate(files_to_read[:1])):
         file_path = os.path.join(folder_path, filename)
@@ -70,16 +80,25 @@ for folder_i, folder_path in tqdm(enumerate(folder_paths)):
 
         # read data
         start = time.perf_counter()
-        with open(file_path, "r") as in_file, open(out_filename, "a") as out_file:
+        
+        with open(out_filename, "a") as out_file:
             start = time.perf_counter()
-            df = pd.read_csv(file_path, sep=',', header=0, skiprows=12,  usecols=[0])
-            genomes = df['Genome'].to_list()
+            df = pd.read_csv(file_path, sep=",", header=0, skiprows=12, usecols=[0])
+            genomes = df["Genome"].to_list()
+            genomes_sample = genomes[:len(genomes)//10]
             print(genomes[:5])
             print(len(genomes))
             print(len(set(genomes)))
+            print(len(genomes_sample))
+            for c1, gene_1 in tqdm(enumerate(genomes_sample)):
+                for c2, gene_2 in enumerate(genomes_sample):
+                    if c1 != c2:
+                        differences_list.append(genome_string_difference_count(gene_1, gene_2, difference_cache))
             time_taken = time.perf_counter() - start
-            print(f"time taken to process file {file_path} = {time_taken:0.3f}s") 
+            print(f"time taken to process file {file_path} = {time_taken:0.3f}s")
 
+# print(differences_list)
+print(len(differences_list))
 # df = pd.read_csv(out_filename)
 # print(df.head())
 # print(len(df))
